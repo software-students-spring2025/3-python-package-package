a3253e1c4d6d4b55099f45dc052f1a0911c1386f
giladspitzer <giladspitzer@gmail.com>
2025-03-15 13:30:58 -0400

    tell_fortune

--- src/magic8ball/fortunes.py
+++ src/magic8ball/fortunes.py
@@ -8,15 +8,15 @@ DATA_PATH = os.path.join(os.path.dirname(__file__), 'data.json')
 with open(DATA_PATH, 'r') as f:
     DATA = json.load(f)
 
+
 class Magic8Ball:
 
     DATA = DATA
-    
+
     @staticmethod
     def tell_fortune() -> str:
         """Return a random general fortune."""
-        # TODO
-        return
+        return random.choice(DATA.get("general"))
 
     @staticmethod
     def personalized_fortune(name: str) -> str:
@@ -27,10 +27,10 @@ class Magic8Ball:
     @staticmethod
     def fortune_by_category(category: str) -> str:
         """Return a fortune from the specified category ('love', 'career', 'health')."""
-        
-        if not isinstance(category,str):
+
+        if not isinstance(category, str):
             raise TypeError("Invalid input: The category must be provided as a string.")
-        
+
         # Retrieve the fortunes under specified category
         fortunes = DATA.get("categories", {}).get(category.lower())
         if fortunes:
@@ -42,13 +42,13 @@ class Magic8Ball:
     def daily_by_birth_month(month: str) -> str:
         """
         Returns a personalized fortune message based on the given birth month.
-        
+
         Parameters:
             month (str): The name of the birth month.
-        
+
         Returns:
             str: A randomly selected fortune message for the given month.
-        
+
         Raises:
             TypeError: If the input is not a string.
             ValueError: If the input is not a valid month name.
@@ -57,11 +57,9 @@ class Magic8Ball:
             raise TypeError("Invalid input: The month must be provided as a string.")
 
         birthdays = DATA["birthdays"]
-        month = month.capitalize()  
+        month = month.capitalize()
 
         if month in birthdays:
-            return random.choice(birthdays[month]) 
+            return random.choice(birthdays[month])
         else:
             raise ValueError("Invalid month: Please enter a valid month name (e.g., 'January', 'February').")
-
-
